3f24b70c656c6f426e4944dd002789891a8690ee
Thomaz Victor de Oliveira <thomazvictorr@gmail.com>
2024-08-07 16:52:29 -0300

    Sair do sistema
    Inclusão da opção 4, sair do sistema

--- portal-do-aluno-ebac/portal-do-aluno.c
+++ portal-do-aluno-ebac/portal-do-aluno.c
@@ -4,8 +4,7 @@
 #include <string.h> // biblioteca responsável por cuidar das string
 
  // ferramenta de consulta, registro e exclusão de alunos em uma plataforma estudantil
-int registro ()
-{
+int registro (){
 	setlocale (LC_ALL, "Portuguese");
 	char arquivo[40];
 	char cpf[40];
@@ -13,6 +12,8 @@ int registro ()
 	char sobrenome[40];
 	char curso [40];
 	
+	printf("####-------------------------####\n");
+	printf("\tPORTAL DO ALUNO\n\n");
 	printf("Digite o CPF a ser cadastrado: ");
 	scanf("%s", cpf);
 	
@@ -64,13 +65,14 @@ int registro ()
 }
 
 
-int consulta()
-{
+int consulta(){
 	setlocale (LC_ALL, "Portuguese");
 	char cpf[40];
 	char conteudo [200];
 	char *token;
 	
+	printf("####-------------------------####\n");
+	printf("\tPORTAL DO ALUNO\n\n");
 	printf("Digite o CPF a ser consultado: ");
 	scanf("%s",cpf);
 
@@ -78,11 +80,15 @@ int consulta()
 	file = fopen(cpf,"r");
 	
 	if(file == 	NULL){
+		printf("####-------------------------####\n");
+		printf("\tPORTAL DO ALUNO\n\n");
 		printf("CPF não localizado... Tente novamente!\n");
 	}
 	
 	while(fgets(conteudo, 200, file) != NULL)
 	{ // caso o usuário esteja no sistema
+		printf("####-------------------------####\n");
+		printf("\tPORTAL DO ALUNO\n\n");
 		printf("\nEssas são as informações do usuário: \n");
 		
 		token = strtok(conteudo, ",");
@@ -102,10 +108,11 @@ int consulta()
 }
 
 
-int deletar()
-{
+int deletar(){
+	setlocale (LC_ALL, "Portuguese");
 	char cpf[40];
-	
+	printf("####-------------------------####\n");
+	printf("\tPORTAL DO ALUNO\n\n");
 	printf("Digite o CPF do aluno a ser deletado: ");
 	scanf("%s",cpf);
 		
@@ -128,22 +135,20 @@ int deletar()
 
 
 int main(){
-	
 	int opcao=0; // definindo variáveis
 	int x=1;
 	
 	for(x=1;x=1;){ //x = laço de repetição
-		
-		system("cls"); // limpatela
 	
 		setlocale (LC_ALL, "Portuguese"); // definindo linguagem
 	
-		printf("##-----------------------------##\n"); // inicio do menu
+		printf("####-------------------------####\n"); // inicio do menu
 		printf("\tPORTAL DO ALUNO\n\n");
 		printf("Escolha a opção desejada do menu:\n");
 		printf("\t1: Registar nomes\n");
 		printf("\t2: Consultar nomes\n");	
 		printf("\t3: Deletar nomes\n"); 
+		printf("\t4: Sair do sistema\n\n");
 		printf("Opção desejada: "); // fim do menu
 	
 		scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -163,8 +168,17 @@ int main(){
 			deletar ();
 			break;
 			
+			case 4:
+			printf("####-------------------------####\n"); // inicio do menu
+			printf("\tPORTAL DO ALUNO\n\n");
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
+			
 			default:
-			printf("Essa opção não está disponível!\n"); // fim da seleção
+			printf("####-------------------------####\n"); // inicio do menu
+			printf("\tPORTAL DO ALUNO\n\n");
+			printf("Essa opção não está disponível!\n\n"); // fim da seleção
 			system("pause");
 			break;			
 		}			
